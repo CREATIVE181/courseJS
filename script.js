d2afc1a8e1d05e1d0f4195be8c56094b8233e49e
CREATIVE181 <30062003@bk.ru>
2024-10-20 02:43:15 +0300

    Callback функции

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-// function showFirstMessage(text) {
-//     alert(text);
-//     let num = 20;
-//     return num;
+// function first() {
+//     setTimeout(function () {
+//         console.log(1);
+//     }, 500);
 // }
 //
-// console.log(showFirstMessage("Hello, World!"));
-
-
-// let func = function(a, b) {
-//     return a + b;
+// function second() {
+//     console.log(2);
 // }
-//
-//
-// console.log(func(1, 2))
 
+// first();
+// second();
 
+function learnJS(lang, callback) {
+    console.log(`Learning ${lang}...`);
+    callback();
+}
 
-// let func = (a, b) => a + b
-// console.log(func(4,5))
-
-
-let str = "TeSt";
-console.log(str);
-
-console.log(str.length);
-
-console.log(str.toUpperCase());
-
-console.log(str.toLowerCase())
-
-let twelve = "12.2px";
-// console.log(Math.round(twelve));
-console.log(parseFloat(twelve));
+learnJS('JavaScript', function() {
+    console.log('JavaScript learning completed!');
+});
