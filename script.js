4713f4100ba2eb943563b92c1235134c45e557d2
CREATIVE181 <30062003@bk.ru>
2024-10-20 02:19:37 +0300

    циклы

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-'use strict';
+let num = 50;
 
+switch (num) {
+    case num < 40:
+        console.log('Number is less than 40');
+        break;
+    case num > 100:
+        console.log('Number is greater than 100');
+        break;
+    case num > 80:
+        console.log('Number is between 80 and 100');
+        break;
+    case 50:
+        console.log('Number is equal to 50');
+        break;
+    default:
+        console.log('Number is not in the specified range');
+        break;
+}
 
-// alert('Hello, World!');
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
 
-// let answerBool = confirm('Are you sure you want to proceed?');
-// console.log(answerBool);
+// do {
+//     console.log(num);
+//     num++;
+// } while (num < 55);
 
-let answerString = prompt('Как вас зовут?', '');
-console.log(answerString);
+for (let i = num; i < 55; i++) {
+    console.log(i);
+}
