1d75372c75d2230bf0f96c93c7fb791db818e8e6
CREATIVE181 <30062003@bk.ru>
2024-10-20 01:31:48 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+// alert('Hello, World!');
+
+// let answerBool = confirm('Are you sure you want to proceed?');
+// console.log(answerBool);
+
+let answerString = prompt('Как вас зовут?', '');
+console.log(answerString);
