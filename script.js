57e9f1e8d9a82decbbc4ab18a27716ba5f8149a8
CREATIVE181 <30062003@bk.ru>
2024-10-20 02:00:13 +0300

    lesson 8

--- script.js
+++ script.js
@@ -6,5 +6,39 @@
 // let answerBool = confirm('Are you sure you want to proceed?');
 // console.log(answerBool);
 
-let answerString = prompt('Как вас зовут?', '');
-console.log(answerString);
+// let answerString = prompt('Как вас зовут?', '');
+// console.log(answerString);
+
+
+// if (2 * 4 !== 8) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+let num = 50;
+
+// if (num < 49) {
+//     console.log('Neverno');
+// } else if (num > 100) {
+//     console.log('Mnogo');
+// } else {
+//     console.log('Normalno');
+// };
+//
+// (num === 50) ? console.log('Ravno') : console.log('Ne ravno');
+
+// switch (num) {
+//     case num < 49:
+//         console.log('Neverno');
+//         break;
+//     case num > 100:
+//         console.log('Mnogo');
+//         break;
+//     case 50:
+//         console.log('Ravno');
+//         break;
+//     default:
+//         console.log('Ne normalno');
+// }
